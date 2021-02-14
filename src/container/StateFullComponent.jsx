83c34d58e8e6540ca8e486e2635b06b04edf0a21
Septian Pratama <septianpratama18@gmail.com>
2021-02-14 14:30:57 +0700

    StateLess Component & Statefull Component

---
+++ src/container/StateFullComponent.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class StateFullComponent extends React.Component{
+    render(){
+        return <p>Statefull Component from folder Container</p>
+    }
+}
+
+export default StateFullComponent;
--- src/index.js
+++ src/index.js
@@ -3,10 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import HelloComponent from "./component/HelloComponent";
+import StateFullComponent from "./container/StateFullComponent";
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <HelloComponent />
   </React.StrictMode>,
   document.getElementById('root')
 );
